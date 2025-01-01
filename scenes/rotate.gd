1472eee944862fd436dec8c18c1584af40481448
Austin Maddison <Austin.madison10152@gmail.com>
2025-01-01 20:37:58 +0000

    .

--- scenes/rotate.gd
+++ scenes/rotate.gd
@@ -1,8 +1,9 @@
 extends Node3D
 
 @export var speed = .5
+@export var strength = .5
 var time = 0;
 
 func _process(delta: float) -> void:
 	time += delta
-	$".".global_rotation_degrees = Vector3(90. + speed * cos(time) * 45, speed * sin(time) * 45, 0.)
+	$".".global_rotation_degrees = Vector3(90. +  strength * cos(speed * time) * 45, speed * sin(speed * time) * 45, 0.)
